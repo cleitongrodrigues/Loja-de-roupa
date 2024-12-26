10e50dee8da5dadb8a93ebe1f1b9ecf13d8d3eb8
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2024-12-26 14:04:02 -0300

    pasta imagem criada e configurada para receber arquivos

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const mysql = require('mysql2');
 const { engine } = require('express-handlebars');
 const app = express();
+const fileupload = require("express-fileupload");
 const port = 3000;
+
+// Habilitando upload de arquivos
+app.use(fileupload());
 
 // Adicionar Bootstrap
 app.use("/bootstrap", express.static("./node_modules/bootstrap/dist"));
@@ -14,6 +18,10 @@ app.use("/css", express.static("./css"));
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+
+// Manipulando dados via rotas
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
 
 // Estabelecendo a conexao com o banco de dados
 const conexao = mysql.createConnection({
@@ -35,5 +43,16 @@ conexao.connect((error) => {
 app.get("/", (req, res) => {
     res.render('formulario');
 });
+
+// Rota de cadastro
+app.post("/cadastrar", (req, res) => {
+    console.log(req.body);
+    console.log(req.files.imagem.name);
+
+    // Diz para qual lugar o arquivo sera enviado
+    req.files.imagem.mv(__dirname + '/imagens/' + req.files.imagem.name);
+    // Evita loop infinito
+    res.end();
+})
 
 app.listen(port);
