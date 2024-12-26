987fdafc27f21ddcafa449876a696f725ccfbe61
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2024-12-26 12:42:55 -0300

    conexao com o banco concluida

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mysql = require('mysql2');
 const app = express();
 const port = 3000;
+
+const conexao = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "loja"
+});
+
+conexao.connect((error) => {
+    if (error){
+        console.log("Erro ao conectar ao banco de dados")
+    return;
+    }
+    console.log("Conexao estabelicida com sucesso")   
+});
 
 app.get("/", (req, res) => {
     res.write("Hello world");
